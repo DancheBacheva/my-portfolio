1ebdc5db6da61c5fd00c8fe026af7c01cde73715
DancheBacheva <danchebacheva@gmail.com>
2024-04-01 01:30:48 +0200

    contact, footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
+import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <div>
+        <div className="contact-container">
+          <div className="contact-info">
+            <img src="src/assets/contact/mobile-icon.png" alt="" />
+            <p>+389 71 743 981</p>
+          </div>
+          <div className="contact-info">
+            <img src="src/assets/contact/gmail-icon.png" alt="" />
+            <p>danchebacheva@gmail.com</p>
+          </div>
+          <div style={{ fontSize: "2rem" }}>
+            <Link to="https://github.com/DancheBacheva" target="_blanc">
+              <FaGithub />
+            </Link>
+            <span style={{ marginRight: "1rem" }}></span>
+            <Link
+              to="https://www.linkedin.com/in/danche-bacheva-b3ab5a93/"
+              target="_blanc"
+            >
+              <FaLinkedin />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import React from "react";
+import "./Footer.css";
 
 export const Footer = () => {
   return (
-    <div style={{ fontSize: '2rem' }}>
-      <Link to='https://github.com/DancheBacheva' target='_blanc'><FaGithub /></Link>
-      <span style={{ marginRight: '1rem' }}></span>
-      <Link to='https://www.linkedin.com/in/danche-bacheva-b3ab5a93/' target='_blanc'>
-      <FaLinkedin /></Link>
-    </div>
-  )
-}
+    <>
+      <div className="copyright">© Danche Bacheva 2024</div>
+    </>
+  );
+};
