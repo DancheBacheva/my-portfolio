ac13c6271d9f47f04e9cb1c4dd6e96a4f6c628fa
DancheBacheva <danchebacheva@gmail.com>
2024-10-03 19:08:35 +0200

    download my CV

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./Contact.css";
 import { Link } from "react-router-dom";
-import { FaGithub, FaLinkedin, FaFileDownload } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaFileDownload } from "react-icons/fa";
 
 export const Contact = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/my_resume.pdf";
+    link.download = "DancheBacheva_CV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <>
       <div>
@@ -17,18 +26,25 @@ export const Contact = () => {
             <p>danchebacheva@gmail.com</p>
           </div>
           <div style={{ fontSize: "2rem" }}>
-            <Link to="https://github.com/DancheBacheva" target="_blanc" style={{ color: 'inherit', textDecoration: 'none' }}>
+            <Link
+              to="https://github.com/DancheBacheva"
+              target="_blanc"
+              style={{ color: "inherit", textDecoration: "none" }}
+            >
               <FaGithub />
             </Link>
             <span style={{ marginRight: "1rem" }}></span>
             <Link
               to="https://www.linkedin.com/in/danche-bacheva-b3ab5a93/"
               target="_blanc"
-              style={{ color: 'inherit', textDecoration: 'none' }}>
+              style={{ color: "inherit", textDecoration: "none" }}
+            >
               <FaLinkedin />
             </Link>
           </div>
-          {/* <button className="download-btn"><FaFileDownload /> Download CV</button> */}
+          <button className="download-btn" onClick={handleDownload}>
+            <FaFileDownload /> Download CV
+          </button>
         </div>
       </div>
     </>
