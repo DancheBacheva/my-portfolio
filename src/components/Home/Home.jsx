dd75d5338c4a67d8b1d318651c4fb645cb40bb3b
DancheBacheva <danchebacheva@gmail.com>
2024-10-03 15:18:28 +0200

    new projects

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,17 +4,23 @@ import "./Home.css";
 export const Home = () => {
   return (
     <>
-    <div className="home-elements">
-      <div className="img-container-home">
-        <div className="photo-flow">
-          <img src="/my-pic.png" alt="me" className="my-photo" />
+      <div className="home-elements">
+        <div className="img-container-home">
+          <div className="photo-flow">
+            <img src="/my-pic.png" alt="me" className="my-photo" />
+          </div>
+        </div>
+        <div className="text-container-home">
+          <h1>DANCHE BACHEVA</h1>
+          <h2>FULL-STACK JAVASCRIPT DEVELOPER</h2>
+          <p>
+            Welcome to my portfolio! Explore my journey as a developer,
+            showcasing my skills, experience, and passion for creating dynamic
+            projects. Check out my latest work, learn more about me, and feel
+            free to get in touch!
+          </p>
         </div>
       </div>
-      <div className="text-container-home">
-        <h1>DANCHE BACHEVA</h1>
-        <h2>FULL-STACK JAVASCRIPT DEVELOPER</h2>
-      </div>
-    </div>
     </>
   );
 };
